76f3622dbad860593288f9bcf4bf714420ce3277
Blaise Taylor <btaylor@bPolicySolutions.com>
2026-06-20 09:27:59 -0400

    Add project files.

---
+++ LogicBuilder.App.Common.Utils/DictionaryHelper.cs
@@ -0,0 +1,21 @@
+﻿using LogicBuilder.App.Common.Utils.Interfaces;
+using LogicBuilder.Forms.Parameters.Expressions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace LogicBuilder.App.Common.Utils
+{
+    public class DictionaryHelper(IMappingOperations mappingOperations) : IDictionaryHelper
+    {
+        private readonly IMappingOperations _mappingOperations = mappingOperations;
+
+        public IDictionary<TKey, TValue> ToDictionary<TSource, TKey, TValue>(IEnumerable<TSource> enumerable, SelectorLambdaOperatorParameters keySelector, SelectorLambdaOperatorParameters valueSelector)
+            => enumerable.ToDictionary
+            (
+                ((Expression<Func<TSource, TKey>>)_mappingOperations.MapToOperator(keySelector).Build()).Compile(),
+                ((Expression<Func<TSource, TValue>>)_mappingOperations.MapToOperator(valueSelector).Build()).Compile()
+            );
+    }
+}
---
+++ LogicBuilder.App.Common.Utils/DictionaryHelperWrapper.cs
@@ -0,0 +1,17 @@
+﻿using LogicBuilder.App.Common.Utils.Interfaces;
+using LogicBuilder.Attributes;
+using LogicBuilder.Forms.Parameters.Expressions;
+using System.Collections.Generic;
+
+namespace LogicBuilder.App.Common.Utils
+{
+#pragma warning disable S2436
+    public static class DictionaryHelperWrapper<TSource, TKey, TValue>//NOSONAR - rules engine supports generic classes but not generic methods
+#pragma warning restore S2436
+    {
+        [AlsoKnownAs("ToDictionary")]
+        [FunctionGroup(FunctionGroup.Standard)]
+        public static IDictionary<TKey, TValue> ToDictionary(IDictionaryHelper dictionaryHelper, IEnumerable<TSource> enumerable, SelectorLambdaOperatorParameters keySelector, SelectorLambdaOperatorParameters valueSelector)
+            => dictionaryHelper.ToDictionary<TSource, TKey, TValue>(enumerable, keySelector, valueSelector);
+    }
+}
